47f5f263b9bae6e13fbc3dd2ef726e21006b751e
warr90 <warrendelfin90@gmail.com>
2025-09-16 11:36:35 +0800

    Update firebaseConfig.js with new Firebase project configuration

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -12,14 +12,13 @@ import {
 import { Platform } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-// ✅ Your actual Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDiSHnZq9sIvGJc6zhWc5N5XAvvhZXmKoU",
-  authDomain: "nexaa-35ef1.firebaseapp.com",
-  projectId: "nexaa-35ef1",
-  storageBucket: "nexaa-35ef1.appspot.com", // ✅ FIXED typo here (was `.firebasestorage.app`)
-  messagingSenderId: "799162817145",
-  appId: "1:799162817145:web:e5cdd64de5b5eb422b52a8",
+  apiKey: "AIzaSyB9azV_a0rex8ZwxDwnuK9VNTEw5rLbhHI",
+  authDomain: "chatwards-5f9be.firebaseapp.com",
+  projectId: "chatwards-5f9be",
+  storageBucket: "chatwards-5f9be.firebasestorage.app",
+  messagingSenderId: "537709732623",
+  appId: "1:537709732623:web:c3fca5582faade8215947a"
 };
 
 // ✅ Initialize app
